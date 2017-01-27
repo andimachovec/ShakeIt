0e0cb95e0fdabfdd0ba40a07c5e7e46887369ada
Andi Machovec <andi.machovec@me.com>
2017-01-27 19:03:51 +0000

    Implementierung von LetterView begonnen, bring Linker Fehler, weiss noch nicht warum

--- src/letterview.h
+++ src/letterview.h
@@ -2,6 +2,12 @@
 #define LETTERVIEW_H
 
 #include <View.h>
+#include <TranslationUtils.h>
+#include <TranslatorFormats.h>
+#include <Bitmap.h>
+
+#include <string>
+#include <vector>
 
 
 class LetterView : public BView
@@ -9,10 +15,17 @@ class LetterView : public BView
 
 	public:
 		LetterView();
-	
+		void Draw(BRect update_rect);
+		void SetLetters(std::vector<std::string> LetterCombination);
+		
+		
 	private:
+		
+		
+		BBitmap *board_letters[16];		
 	
 	
+		
 };	
 
 
