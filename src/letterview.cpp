e0ad647eb3c83d48f7fed1df9aa0ecdf8e3fd364
Andi Machovec <andi.machovec@me.com>
2017-01-28 13:03:57 +0000

    LetterView Grundfunktionen alle implementiert

--- src/letterview.cpp
+++ src/letterview.cpp
@@ -6,17 +6,104 @@ LetterView::LetterView()
 		: BView("letterview", B_SUPPORTS_LAYOUT|B_WILL_DRAW)
 //-----------------------------------------------------------------------------
 {
+		
+	for (int i=0; i<16; ++i)
+	{
+	
+		letter_filenames[i]="empty.png";	
+		
+	}	
+	
+	load_bitmaps();
+	draw_bitmaps();
+}	
+
+
+
+//-----------------------------------------------------------------------------
+void LetterView::Draw(BRect update_rect)
+//-----------------------------------------------------------------------------
+{
+	
+	draw_bitmaps();
 	
 	
 }	
 
 
+
 //-----------------------------------------------------------------------------
-void LetterView::SetLetters(std::vector<std::string> LetterCombination)
+void LetterView::SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation)
 //-----------------------------------------------------------------------------
 {
 
+	for (int i=0; i<16; ++i)
+	{
+		
+		std::stringstream filename_stream;
+		filename_stream << Letters[i] << "-" << LetterOrientation[i] << ".png";
+		
+		letter_filenames[i]=filename_stream.str();
+		
+	}	 
+	
+	load_bitmaps();
+	draw_bitmaps();
+	
+	
+}	
 
+
+
+//-----------------------------------------------------------------------------
+void LetterView::load_bitmaps()
+//-----------------------------------------------------------------------------
+{
+
+	for (int i=0; i<16; ++i)
+	{
+		std::string bitmap_name="../"+letter_filenames[i];
+		board_letters[i] = BTranslationUtils::GetBitmap(bitmap_name.c_str());		
+	}	 
+
+}	
+
+
+
+//-----------------------------------------------------------------------------
+void LetterView::draw_bitmaps()
+//-----------------------------------------------------------------------------
+{
+
+	SetDrawingMode(B_OP_ALPHA);
+	//SetHighColor(255,255,255);
+	//FillRect(Bounds());
 	
 	
+	int x_start=80;
+	int y_start=40;
+	int x_delta=80;
+	int y_delta=80;
+	
+	int letter_index=0;
+	int draw_coord_x=x_start;
+	int draw_coord_y=y_start;
+	
+	
+	for (int rows=0; rows<4; ++rows)
+	{
+		for (int cols=0; cols<4; ++cols)
+		{
+			MovePenTo(draw_coord_x,draw_coord_y);
+			DrawBitmap(board_letters[letter_index]); 		
+			++letter_index;
+			draw_coord_x+=x_delta;
+		}
+	
+	
+		draw_coord_x=x_start;
+		draw_coord_y+=y_delta;
+		
+	}
+		 
 }	
--- src/letterview.h
+++ src/letterview.h
@@ -5,10 +5,13 @@
 #include <TranslationUtils.h>
 #include <TranslatorFormats.h>
 #include <Bitmap.h>
+#include <Layout.h>
+#include <LayoutItem.h>
+#include <GridLayout.h>
 
 #include <string>
 #include <vector>
-
+#include <sstream>
 
 class LetterView : public BView
 {
@@ -16,14 +19,16 @@ class LetterView : public BView
 	public:
 		LetterView();
 		void Draw(BRect update_rect);
-		void SetLetters(std::vector<std::string> LetterCombination);
+		void SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation);
 		
 		
 	private:
-		
-		
+			
+		void load_bitmaps();
+		void draw_bitmaps();
+				
 		BBitmap *board_letters[16];		
-	
+		std::string letter_filenames[16];
 	
 		
 };	
