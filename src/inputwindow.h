975feadae156fa674729e9fb38d0bb9bd8854736
Andi Machovec <andi.machovec@me.com>
2017-01-28 13:08:00 +0000

    InputWindow: Im Header Methodendefinitionen aus der Wx Version übernommen (noch auskommentiert)

--- src/inputwindow.h
+++ src/inputwindow.h
@@ -7,13 +7,20 @@
 #include <Layout.h>
 #include <LayoutItem.h>
 
+#include <string>
+#include <vector>
+
 
 class InputWindow : public BWindow
 {
 	
 	public:
 		InputWindow();
-	
+		//void ClearText(void);
+		//void SetTextActive(void);
+		//void SetTextInactive(void);
+		//std::vector<std::string> GetWordList(void);
+		//void DisplayResults(round_results results, int points_current_round, std::vector<std::string> missing_words);
 	
 	private:
 		BTextView *words_textview;
