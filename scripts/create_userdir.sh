2c5d58bd1004c00f5c248658844eeca22176be7c
Andi Machovec <andi.machovec@me.com>
2017-02-13 15:48:59 +0000

    Script zum Anlegen des User Einstellungsverzeichnis erstellt

---
+++ scripts/create_userdir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -v -p /boot/home/config/settings/Boggle
+cp -v ../data/boggle.xml /boot/home/config/settings/Boggle
+cp -v ../data/dictionary.txt /boot/home/config/settings/Boggle
