0c1d1b4f58557ebb5817165b496f454fd279eb1c
Andi Machovec <andi.machovec@me.com>
2017-12-11 15:29:34 +0100

    added class ButtonView

---
+++ src/buttonview.h
@@ -0,0 +1,19 @@
+#ifndef BUTTONVIEW_H
+#define BUTTONVIEW_H
+
+
+#include <View.h>
+#include <Button.h>
+
+
+class ButtonView : public BView
+{
+
+	public:
+		ButtonView();
+
+	private:
+
+};
+
+#endif
