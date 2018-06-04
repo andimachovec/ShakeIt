20456c7f5b903c1ee3c35b3bdfa842308fce3f2d
Andi Machovec <andi.machovec@me.com>
2018-06-04 19:30:13 +0200

    defs.h: updated copyright year and bumped up version number

--- src/defs.h
+++ src/defs.h
@@ -6,10 +6,10 @@
 
 //App Settings
 #define APPTITLE		 "Boggle" 
-#define APPVERSION		 "0.6.2" 
+#define APPVERSION		 "0.7.1" 
 #define APPAUTHOR		 "Andi Machovec"	
 #define APPCOPYRIGHTNAME "Andi Machovec"
-#define APPCOPYRIGHTYEAR 2017
+#define APPCOPYRIGHTYEAR 2018
 #define APPDESCRIPTION    "a word searching game"
 #define APPEXTRAINFO	  ""
 #define APPSPECIALTHANKS  "me and everybody else"
