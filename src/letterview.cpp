edf102b8f3a8eea7cf452a5d9257cfefe33223f2
Andi Machovec <andi.machovec@me.com>
2018-10-11 23:21:53 +0200

    LetterView: converted to Haiku conding style

--- src/letterview.cpp
+++ src/letterview.cpp
@@ -1,16 +1,24 @@
 #include "letterview.h"
 
+#include <TranslationUtils.h>
+#include <TranslatorFormats.h>
+#include <Layout.h>
+#include <LayoutItem.h>
+#include <GridLayout.h>
 
-//-----------------------------------------------------------------------------
+#include <sstream>
+#include <iostream>
+
+
 LetterView::LetterView()
-		: BView("letterview", B_SUPPORTS_LAYOUT|B_WILL_DRAW)
-//-----------------------------------------------------------------------------
+		:
+		BView("letterview", B_SUPPORTS_LAYOUT|B_WILL_DRAW)
 {
 	
 	for (int i=0; i<16; ++i)
 	{
 	
-		letter_filenames[i]="empty.png";	
+		fLetterFilenames[i]="empty.png";	
 		
 	}	
 	
@@ -19,25 +27,20 @@ LetterView::LetterView()
 }	
 
 
-
-//-----------------------------------------------------------------------------
-void LetterView::Draw(BRect update_rect)
-//-----------------------------------------------------------------------------
+void
+LetterView::Draw(BRect update_rect)
 {
 	
 	SetHighColor(242,188,44);
 	FillRect(update_rect);
 	
 	draw_bitmaps();
-	
-	
+		
 }	
 
 
-
-//-----------------------------------------------------------------------------
-void LetterView::SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation)
-//-----------------------------------------------------------------------------
+void
+LetterView::SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation)
 {
 
 	for (int i=0; i<16; ++i)
@@ -46,7 +49,7 @@ void LetterView::SetLetters(std::vector<std::string> Letters, std::vector<int> L
 		std::stringstream filename_stream;
 		filename_stream << Letters[i] << "-" << LetterOrientation[i] << ".png";
 		
-		letter_filenames[i]=filename_stream.str();
+		fLetterFilenames[i]=filename_stream.str();
 		
 	}	 
 	
@@ -57,24 +60,19 @@ void LetterView::SetLetters(std::vector<std::string> Letters, std::vector<int> L
 }	
 
 
-
-//-----------------------------------------------------------------------------
-void LetterView::load_bitmaps()
-//-----------------------------------------------------------------------------
+void
+LetterView::load_bitmaps()
 {
 
 	for (int i=0; i<16; ++i)
 	{
-		board_letters[i] = BTranslationUtils::GetBitmap(B_PNG_FORMAT,letter_filenames[i].c_str());		
+		fBoardLetters[i] = BTranslationUtils::GetBitmap(B_PNG_FORMAT,fLetterFilenames[i].c_str());		
 	}	 
 
 }	
 
 
-
-//-----------------------------------------------------------------------------
 void LetterView::draw_bitmaps()
-//-----------------------------------------------------------------------------
 {
 
 	SetDrawingMode(B_OP_ALPHA);
@@ -82,8 +80,8 @@ void LetterView::draw_bitmaps()
 	
 	int view_width=static_cast<int>(Frame().Width());
 	int view_height=static_cast<int>(Frame().Height());
-	int bitmap_width=static_cast<int>(board_letters[0]->Bounds().Width());
-	int bitmap_height=static_cast<int>(board_letters[0]->Bounds().Height());
+	int bitmap_width=static_cast<int>(fBoardLetters[0]->Bounds().Width());
+	int bitmap_height=static_cast<int>(fBoardLetters[0]->Bounds().Height());
 
 
 	int x_delta=70;
@@ -93,10 +91,7 @@ void LetterView::draw_bitmaps()
 		
 	//std::cout << "x_delta: " << x_delta << std::endl;
 	
-	
-	
-	
-	
+		
 	int letter_index=0;
 	int draw_coord_x=x_start;
 	int draw_coord_y=y_start;
@@ -107,7 +102,7 @@ void LetterView::draw_bitmaps()
 		for (int cols=0; cols<4; ++cols)
 		{
 			MovePenTo(draw_coord_x,draw_coord_y);
-			DrawBitmap(board_letters[letter_index]); 		
+			DrawBitmap(fBoardLetters[letter_index]); 		
 			++letter_index;
 			draw_coord_x+=x_delta;
 		}
--- src/letterview.h
+++ src/letterview.h
@@ -2,38 +2,28 @@
 #define LETTERVIEW_H
 
 #include <View.h>
-#include <TranslationUtils.h>
-#include <TranslatorFormats.h>
 #include <Bitmap.h>
-#include <Layout.h>
-#include <LayoutItem.h>
-#include <GridLayout.h>
 
 #include <string>
 #include <vector>
-#include <sstream>
-#include <iostream>
 
-class LetterView : public BView
-{
 
-	public:
-		LetterView();
-		void Draw(BRect update_rect);
-		void SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation);
-		
-		
-	private:
-			
-		void load_bitmaps();
-		void draw_bitmaps();
-				
-		BBitmap *board_letters[16];		
-		std::string letter_filenames[16];
+class LetterView : public BView {
+public:
 	
+	LetterView();
+	void Draw(BRect update_rect);
+	void SetLetters(std::vector<std::string> Letters, std::vector<int> LetterOrientation);
+		
+private:
+			
+	void load_bitmaps();
+	void draw_bitmaps();
+				
+	BBitmap *fBoardLetters[16];		
+	std::string fLetterFilenames[16];
 		
 };	
-
 
 
 #endif 
