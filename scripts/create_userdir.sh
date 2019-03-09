069f460e1baa8026a5488e0be5b546d93cb5df4a
Andi Machovec <andi.machovec@me.com>
2019-03-09 13:16:42 +0100

    updated the script to install data files and the files in the data directory

--- scripts/create_userdir.sh
+++ scripts/create_userdir.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -v -p /boot/home/config/settings/Boggle
-mkdir -v /boot/home/config/settings/Boggle/dict
-cp -v ../data/boggle.xml /boot/home/config/settings/Boggle
-cp -v ../data/dict/*.dict /boot/home/config/settings/Boggle/dict
+SETTINGSDIR=/boot/home/config/settings/ShakeIt
+
+mkdir -v -p $SETTINGSDIR
+cp -Rv ../data/* $SETTINGSDIR
+
+
